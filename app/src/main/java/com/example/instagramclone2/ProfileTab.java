7bc4a874379e1d17bb5076fc69bfa2b6eaa6a19e
Aliasgar Nasir <nasiraliasgar@gmail.com>
2020-04-18 04:41:54 +0530

    Worked on the ProfileTab

--- app/src/main/java/com/example/instagramclone2/ProfileTab.java
+++ app/src/main/java/com/example/instagramclone2/ProfileTab.java
@@ -1,5 +1,6 @@
 package com.example.instagramclone2;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
 
 /**
@@ -15,6 +24,9 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ProfileTab extends Fragment {
+
+    private EditText edtProfileName,edtProfileBio,edtProfileProfession,edtProfileHobby,edtProfileSport;
+    private Button btnUpdateInfo;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -59,6 +71,72 @@ public class ProfileTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_tab, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        edtProfileName = view.findViewById(R.id.edtProfileName);
+        edtProfileBio = view.findViewById(R.id.edtProfileBio);
+        edtProfileProfession = view.findViewById(R.id.edtProfileProfession);
+        edtProfileHobby = view.findViewById(R.id.edtProfileHobby);
+        edtProfileSport = view.findViewById(R.id.edtProfileSport);
+        btnUpdateInfo = view.findViewById(R.id.btnUpdateInfo);
+
+        final ParseUser parseUser = ParseUser.getCurrentUser();
+
+        if(parseUser.get("profileName") == null){
+            edtProfileName.setText("");
+        }else{
+            edtProfileName.setText(parseUser.get("profileName").toString());
+        }
+        if (parseUser.get("profileBio") == null){
+            edtProfileBio.setText("");
+        }else{
+            edtProfileBio.setText(parseUser.get("profileBio").toString());
+        }
+        if ( parseUser.get("profileProfession") == null){
+            edtProfileProfession.setText("");
+        }else{
+            edtProfileProfession.setText(parseUser.get("profileProfession").toString());
+        }
+        if (parseUser.get("profileHobby") == null){
+            edtProfileHobby.setText("");
+        }else{
+            edtProfileHobby.setText(parseUser.get("profileHobby").toString());
+        }
+        if(parseUser.get("profileSport") == null){
+            edtProfileSport.setText("");
+        }else {
+            edtProfileSport.setText(parseUser.get("profileSport").toString());
+        }
+
+
+
+        btnUpdateInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                parseUser.put("profileName", edtProfileName.getText().toString());
+                parseUser.put("profileBio", edtProfileBio.getText().toString());
+                parseUser.put("profileProfession",edtProfileProfession.getText().toString());
+                parseUser.put("profileHobby", edtProfileHobby.getText().toString());
+                parseUser.put("profileSport",edtProfileSport.getText().toString());
+
+                ProgressDialog progressDialog = new ProgressDialog(getContext());
+                progressDialog.setMessage("Updating Info...");
+                progressDialog.show();
+                parseUser.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e == null){
+                            FancyToast.makeText(getContext(), "Info Updated.", FancyToast.LENGTH_SHORT, FancyToast.INFO, false).show();
+                        }else {
+                            FancyToast.makeText(getContext(), e.getMessage(), FancyToast.LENGTH_SHORT, FancyToast.ERROR, false).show();
+
+                        }
+                    }
+                });
+                progressDialog.dismiss();
+            }
+        });
+
+        return view;
     }
 }
